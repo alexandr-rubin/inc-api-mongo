32fce6198082fe2bc2fc02144f770a3b23a2a50a
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-07-29 18:34:13 +0300

    fix resend email throw errors

--- src/domain/authorization.service.ts
+++ src/domain/authorization.service.ts
@@ -49,11 +49,11 @@ export class AuthorizationService {
     const user = await this.userQueryRepository.getUsergByEmail(email)
     // middlware
     if (!user)
-      throw new BadRequestException()
-    if (user.confirmationEmail.isConfirmed === false)
-      throw new BadRequestException()
+      throw new BadRequestException('User not found')
+    if (user.confirmationEmail.isConfirmed)
+      throw new BadRequestException('User is confirmed')
     if(new Date(user.confirmationEmail.expirationDate) < new Date()){
-      throw new BadRequestException()
+      throw new BadRequestException('Token is expired')
     }
     const code = uuidv4()
     user.confirmationEmail.confirmationCode = code
