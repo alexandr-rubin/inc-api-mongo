5962c7471e1530740cc1fa6c45b2da0c22286bb0
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-08-02 14:19:07 +0300

    change bcrypt import

--- src/helpers/generateHash.ts
+++ src/helpers/generateHash.ts
@@ -1,9 +1,9 @@
-import { genSalt, hash } from 'bcrypt'
+import bcrypt from 'bcrypt'
 
 export async function generateHash(password: string) {
-  const salt = await genSalt(10)
+  const salt = await bcrypt.genSalt(10)
   console.log(salt)
-  const passHash = await hash(password, salt)
+  const passHash = await bcrypt.hash(password, salt)
   console.log('hash: ' + passHash)
   return passHash
 }
