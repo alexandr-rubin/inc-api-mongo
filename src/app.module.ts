d4005b523218462a0ee29d8ae0e374191158d3a4
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-08-04 16:13:39 +0300

    acc token 10s. return LogAPIThrottlerGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -99,7 +99,7 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'secretkey'
     JwtModule.register({
       global: true,
       secret: JWT_SECRET_KEY,
-      signOptions: { expiresIn: '5m' },
+      signOptions: { expiresIn: '10s' },
     }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(MONGODB_URI),
