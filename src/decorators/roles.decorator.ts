1de432c05113fcb7a3cd1cdd4e8719d0c8ecf816
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-08-18 12:38:05 +0300

    fix imports roles

--- src/decorators/roles.decorator.ts
+++ src/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
-import { UserRoles } from 'src/helpers/userRoles';
+import { UserRoles } from '../helpers/userRoles';
 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: UserRoles[]) => SetMetadata(ROLES_KEY, roles);
--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from 'src/decorators/roles.decorator';
-import { UserRoles } from 'src/helpers/userRoles';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { UserRoles } from '../helpers/userRoles';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
