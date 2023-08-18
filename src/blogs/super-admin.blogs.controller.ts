28bc2ed0c0275897eefd38eeeaf87906d961158e
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-08-18 14:07:40 +0300

    remove sa basic auth

--- src/blogs/super-admin.blogs.controller.ts
+++ src/blogs/super-admin.blogs.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Get, Param, Put, Query, UseGuards } from "@nestjs/common";
+import { Controller, Get, Param, Put, Query } from "@nestjs/common";
 import { BlogService } from "./blog.service";
 import { QueryParamsModel } from "../models/PaginationQuery";
 import { BlogQueryRepository } from "./blog.query-repository";
 import { PostQueryRepository } from "../posts/post.query-repository";
-import { BasicAuthGuard } from "../guards/basic-auth.guard";
 import { UserQueryRepository } from "../users/user.query-repository";
 import { BlogIdValidationPipe } from "../validation/pipes/blog-Id-validation.pipe";
 import { UserIdValidationPipe } from "../validation/pipes/user-Id-validation.pipe";
 
-@UseGuards(BasicAuthGuard)
 @Controller('sa/blogs')
 export class SuperAdminBlogsController {
   constructor(private readonly blogQueryRepository: BlogQueryRepository, private readonly blogService: BlogService, private readonly postQueryRepository: PostQueryRepository,
